345fd3ad5fc5238d19495fb738f6bccddf570dc0
Trikolon <git@pbz.im>
2019-12-06 21:46:18 +0100

    1.0.0 Release

--- src/main/java/com/tallcraft/clawmachine/ClawMachine.java
+++ src/main/java/com/tallcraft/clawmachine/ClawMachine.java
@@ -1,12 +1,14 @@
 package com.tallcraft.clawmachine;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,6 +26,8 @@ public final class ClawMachine extends JavaPlugin implements Listener {
 
         defaultConfig.set("unsafeWorlds", new ArrayList<>());
         defaultConfig.set("safeDestinationWorld", "world");
+        defaultConfig.set("teleportMsg", "&6You have left &9{{UnsafeWorldName}}&6.");
+        defaultConfig.set("unsafeWorldEnterMsg", "&6You are entering &9{{UnsafeWorldName}}&6. Please note that you will automatically leave this world on logout.");
 
         config.setDefaults(defaultConfig);
         config.options().copyDefaults(true);
@@ -36,9 +40,13 @@ public final class ClawMachine extends JavaPlugin implements Listener {
         initConfig();
 
         String safeWorldName = config.getString("safeDestinationWorld");
+        if (safeWorldName == null) {
+            getLogger().warning("Missing safeDestinationWorld name! Please check the configuration file");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
 
         safeWorld = Bukkit.getServer().getWorld(safeWorldName);
-        if(safeWorld == null) {
+        if (safeWorld == null) {
             getLogger().warning("Invalid safeDestinationWorld '" + safeWorldName + "'!");
             Bukkit.getPluginManager().disablePlugin(this);
         }
@@ -46,18 +54,42 @@ public final class ClawMachine extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
+    @EventHandler
+    public void onPlayerChangedWorldEvent(PlayerChangedWorldEvent event) {
+        String worldEnterMsg = config.getString("unsafeWorldEnterMsg");
+
+        if (worldEnterMsg != null && !worldEnterMsg.isEmpty()) {
+            Player player = event.getPlayer();
+            String destWorldName = player.getWorld().getName();
+            if (config.getStringList("unsafeWorlds").contains(destWorldName)) {
+                String msg = ChatColor.translateAlternateColorCodes('&',
+                        worldEnterMsg.replace("{{UnsafeWorldName}}", destWorldName));
+                player.sendMessage(msg);
+            }
+        }
+    }
+
 
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        boolean isUnsafe = config.getStringList("unsafeWorlds").contains(player.getWorld().getName());
+        String currentWorldName = player.getWorld().getName();
+        boolean isUnsafe = config.getStringList("unsafeWorlds").contains(currentWorldName);
 
-        if(!isUnsafe) {
+        if (!isUnsafe) {
             return;
         }
 
-        // TODO: teleport msg from config
+        String configMsg = config.getString("teleportMsg");
+        String msg = null;
+        if (configMsg != null && !configMsg.isEmpty()) {
+            msg = configMsg.replace("{{UnsafeWorldName}}", currentWorldName);
+            msg = ChatColor.translateAlternateColorCodes('&', msg);
+        }
         player.teleport(safeWorld.getSpawnLocation());
+        if (msg != null) {
+            player.sendMessage(msg);
+        }
     }
 
 }
